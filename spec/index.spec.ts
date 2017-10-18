d3bc9c736b016ef08be31b2d832633949a937774
profiprog <milan.skuhra@profiprog.com>
2017-10-18 09:01:37 +0200

    test coverage

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,7 +1,13 @@
-import { strgen } from "../src/index"
+import { strgen, padding } from "../src/index"
 
 describe("index.ts:", function () {
+
 	it("strgen", function () {
 		expect(strgen("*", 8)).toBe("********");
-	})
+	});
+
+	it("padding", function () {
+		expect(padding("50€", 10)).toBe("       50€");
+		expect(padding("Suma:", -10)).toBe("Suma:     ");
+	});
 });
